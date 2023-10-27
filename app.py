0f34688647952727d7d1567ee51e2f7f80e8c718
Jack291199 <148333062+Jack291199@users.noreply.github.com>
2023-10-27 07:40:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def load_lottieURL(url):
 # load assets
 lottie_coding = load_lottieURL("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
 
-img_contact_form = Image.open("C:\\Users\\Administrator\\Desktop\\website\\image\\python.png")
+img_contact_form = Image.open("https://github.com/Jack291199/cty1/blob/8265d3258031363ce8820536275857ae0a6ff430/image/python.png")
 
 img_contact_form = img_contact_form.resize((450,280))
 
